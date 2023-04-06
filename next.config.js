f46394636965ddb5196f21c1d7894948b970510f
The_pichard <ing.sergio.pichardo.a@gmail.com>
2023-04-06 01:23:30 -0600

    se configuro la privacidad de usar imagenes de otros citios

--- next.config.js
+++ next.config.js
@@ -4,3 +4,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ['https://scontent.fmex28-1.fna.fbcdn.net/v/t39.30808-6/317494435_5695746437173338_5107786524602904042_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeESEaEhfB3vt24rqwt5CuHHOciCOpr3ldQ5yII6mveV1N7dm13oC22xF7HPJG5py_HbbdFiZ27xe1ewEdlA9jDY&_nc_ohc=NyHU_S-AKzMAX86cIWg&_nc_ht=scontent.fmex28-1.fna&oh=00_AfBYrAtvhoyI93QNBeWp7dMyfdOT5ursISSpO0g_-OSj7w&oe=6432F2C6'],
+  },
+}
