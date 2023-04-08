151b985f3f1f87e176c8c0ccc95c45183c49cc2f
The_pichard <ing.sergio.pichardo.a@gmail.com>
2023-04-07 18:28:59 -0600

    se creo un documento css para poder poner la imagen como hover por medio de el ID y se importo el documento

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@components/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../styles/globals.css";
+import "../styles/button.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
